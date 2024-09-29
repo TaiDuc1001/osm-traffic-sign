495a8c61690307a756a68c4e7ddb26aec6062d26
taiduc1001 <phantaiduc2005@gmail.com>
2024-09-29 13:53:23 +0700

    Feat: cafe to traffic sign

--- utils/upload.py
+++ utils/upload.py
@@ -1,14 +1,28 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import argparse
-from utils.utils import create_changeset, create_node, close_changeset, \
+from utils import create_changeset, create_node, close_changeset, \
                     gen_comment
+
+TRAFFIC_SIGNS = {
+    'stop': 'stop',
+    'yield': 'give_way',
+    'speed_limit': 'maxspeed',
+    'no_entry': 'no_entry',
+    'one_way': 'oneway',
+    'no_parking': 'no_parking',
+    'crosswalk': 'crossing',
+    'traffic_light': 'traffic_signals'
+}
 
 def main(**kwargs):
     headers = kwargs.get('headers', None)
     api_endpoint = kwargs.get('api_endpoint', None)
     lat = kwargs.get('lat', None)
     lon = kwargs.get('lon', None)
-    name = kwargs.get('name', None)
-    comment = gen_comment(type='Stop', lat=lat, lon=lon)
+    sign_type :str = kwargs.get('sign_type', None)
+    comment = gen_comment(type=f'Traffic Sign: {sign_type.capitalize()}', lat=float(lat), lon=float(lon))
 
     try:
         changeset_id = create_changeset(
@@ -19,10 +33,15 @@ def main(**kwargs):
         print(f"Changeset created with ID: {changeset_id}")
 
         tags = {
-            'name': name,
-            'amenity': 'cafe',
-            'description': 'A sample point of interest'
+            'highway': 'traffic_sign',
+            'traffic_sign': TRAFFIC_SIGNS.get(sign_type, 'unknown')
         }
+
+        if sign_type == 'speed_limit':
+            tags['maxspeed'] = kwargs.get('speed_limit', '50')
+        elif sign_type == 'one_way':
+            tags['oneway'] = 'yes'
+
         node_id = create_node(
             changeset_id=changeset_id, 
             lat=lat, lon=lon, tags=tags, 
@@ -40,9 +59,10 @@ def main(**kwargs):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--use-dev-api', action='store_true', help='Use the development API')
-    parser.add_argument('--lat', type=float, required=True)
-    parser.add_argument('--lon', type=float, required=True)
-    parser.add_argument('--name', type=str, required=True)
+    parser.add_argument('--lat', type=str, required=True)
+    parser.add_argument('--lon', type=str, required=True)
+    parser.add_argument('--sign-type', type=str, required=True, choices=TRAFFIC_SIGNS.keys())
+    parser.add_argument('--speed-limit', type=str, default='50')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -61,5 +81,6 @@ if __name__ == '__main__':
         lat=args.lat, 
         lon=args.lon, 
         api_endpoint=API_ENDPOINT,
-        name=args.name
+        sign_type=args.sign_type,
+        speed_limit=args.speed_limit
     )

bcc80a245cba0a6f354cf6b2d796e65d549fb90f
taiduc1001 <phantaiduc2005@gmail.com>
2024-09-29 11:01:14 +0700

    Feat: add use_dev_api & get_data_json

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import os
-def get_access_token() -> str:
+import argparse
+import json
+
+def get_access_token(use_dev_api: bool) -> str:
     command = f'''
         python get_access_token.py
     '''
+    if use_dev_api:
+        command += ' --use-dev-api'
     return command
 
-def upload(lat, lon, name) -> str:
+def upload(lat: float, lon:float, name: str, use_dev_api: bool) -> str:
     command = f'''
         python upload.py --lat {lat} --lon {lon} --name {name}
     '''
+    if use_dev_api:
+        command += ' --use-dev-api'
     return command
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-dev-api', action='store_true', help='Use the development API')
+    parser.add_argument('--data-json', type=str, required=True, help='Path to the JSON file containing the data')
+    return parser.parse_args()
+
+def get_data_from_json(data_json):
+    with open(data_json, 'r') as f:
+        data = json.load(f)
+    return data
+
 if __name__ == '__main__':
-    os.system(get_access_token())
-    lat = 37.7749
-    lon = -122.4194
-    name = 'Example of Traffic Sign'
-    os.system(upload(lat, lon, name))
+    args = parse_args()
+    os.system(get_access_token(args.use_dev_api))
+    data = get_data_from_json(args.data_json)
+    for item in data:
+        lat = item.get('lat')
+        lon = item.get('lon')
+        name = item.get('name')
+        os.system(upload(lat, lon, name))
